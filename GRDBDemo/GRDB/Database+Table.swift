34f7067cdfd46ed0ddd4bb89bf1fccf32408eff7
Cheng-Hong <s26803018@gmail.com>
2022-11-08 10:37:09 +0800

    fix common

--- GRDBDemo/GRDB/Database+Table.swift
+++ GRDBDemo/GRDB/Database+Table.swift
@@ -8,40 +8,23 @@
 import Foundation
 import GRDB
 
-/* 關聯使用範例 */
+/* 關聯 */
 
-// (1) 建表拉關聯 Entity.databaseTableName -> 你要關聯的表，column: 你要關聯的值，onDelete: 要刪除的規則
+// 1. .references (Association.Columns.entityID 關聯到 Entity.Columns.id)
+//t.column(Association.Columns.entityID.name)
+//    .references(Entity.databaseTableName, onDelete: .none)
 
-// (2) Associations 底下創建struct
-
-// (3) 創建要求: belongsTo: 子表屬於哪張主表(Entity.self)，這邊示範的是子表，如果你是主表則會用hasMany or hasOne 取決於你這張表底下有幾個子元素
-
-// (4) 創建變數: 塞入剛剛創建的條件取出主表
-
-//private func createAssociationTable() throws {
-//    try self.create(table: Association.databaseTableName) { t in
-//        t.autoIncrementedPrimaryKey(Association.Columns.id.name)
-//
-//        // (1) 在你要關聯的值設定 .references
-//        t.column(Association.Columns.entityID.name)
-//            .notNull()
-//            .indexed()
-////                .references(Entity.databaseTableName, column: Entity.Columns.id, onDelete: .none)
-//            .references(Entity.databaseTableName, onDelete: .none)
-//    }
-//}
-
-// (2) 子表
+// 2. Associations folder 底下創建關聯Model
 //struct AssociationInfo: FetchableRecord, Decodable {
 //    let associaiton: Association
 //    let entity: Entity
 //}
 //
 //extension Association {
-//    // (3)
+//    // 3. 創建要求: belongsTo: 子表屬於哪張主表(Entity.self)，這邊示範的是子表，如果你是主表則會用hasMany or hasOne 取決於你這張表底下有幾個子元素
 //    static let entity = belongsTo(Entity.self)
 //
-//    // (4)
+//    // 4. 創建變數: 塞入剛剛創建的條件取出主表
 //    var entity: QueryInterfaceRequest<Entity> {
 //        request(for: Association.entity)
 //    }
